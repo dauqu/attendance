3fba23e12772e877cc591fa92a08150757743105
Harsh Singh <sujeetsingh734321@gmail.com>
2023-05-04 19:27:08 +0530

    chnages

--- lib/components/splash_screen.dart
+++ lib/components/splash_screen.dart
@@ -1,7 +1,5 @@
 import 'package:attendance/components/register.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
